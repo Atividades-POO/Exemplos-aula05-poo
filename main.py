c5d5f630d7d69524e4be84e723ba44efeb7d83ad
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-07 21:45:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,17 @@
 # Carlos Eduardo
 # data: 07/07/2022
 #
+# importando a class Usuario do arquivo ex01.py
+from ex01 import Usuario # importando a class Usuario do arquivo ex01.py
+
+# instanciando a classe Usuario
+u1 = Usuario('Davi', '123456')
+print(f'Nome: {u1.nome}')
+print("logando no sistema...")
+u1.login = True
+print("_________________________________________________")
+
+print("saindo do sistema...")
+u1.login = False
+
+
